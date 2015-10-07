627c796f4e1692c6c19bc25251f631dc8253cf6a
Glenn Hickey <hickey@soe.ucsc.edu>
2015-10-07 08:57:43 -0700

    fix call_snps script

--- call_snps.sh
+++ call_snps.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-	 echo "Syntax $0 <graphs_dir> <calls_dir> <out_dir>"
+	 echo "Syntax $0 <graphs_dir> <alignments_dir> <out_dir>"
 	 exit 1
 fi
 
 GRAPHS=$1
 ALIGNMENTS=$2
-VARIANTS=$3
-OUT_DIR=$4
+OUT_DIR=$3
 
-OPTS="--maxCores 24 --kmer 27 --edge_max 7 --vg_cores 2"
+OPTS="--maxCores 24 --vg_cores 2"
 
 mkdir -f $OUT_DIR
 
-for i in brca1 brca2 sma lrc_kr mhc cenx
+for i in brca1 brca2 sma lrc_kir mhc cenx
 do
-	 rm -rf blan123 ; ./callVariants.py ./blan123 ${ALIGNMENTS}/${i}/*.gam --graph_dir ${GRAPHS} --out_dir ${VARIANTS} ${OPTS}
+	 rm -rf blan123 ; ./callVariants.py ./blan123 ${ALIGNMENTS}/${i}/*/*.gam --graph_dir ${GRAPHS} --out_dir ${OUT_DIR} ${OPTS}
 done
